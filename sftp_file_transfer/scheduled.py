5eb31ae51ced1a911266c620de32e094ffba67b1
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2025-11-14 16:08:43 -0300

    FIX: filter files

--- sftp_file_transfer/scheduled.py
+++ sftp_file_transfer/scheduled.py
@@ -27,6 +27,7 @@ group = Group()
     ),
 )
 def scheduled_task():
+    print('Starting scheduled SFTP file transfer...')
     try:
         env = EnvLoader()
         config = SFTPManagerConfig(
@@ -37,27 +38,35 @@ def scheduled_task():
             key_filepath=None,
             key_password=None,
         )
-        local_dir = os.getenv('LOCAL_PATH')
+        local_dir_list = os.getenv('LOCAL_PATH')
         remote_dir = os.getenv('REMOTE_PATH')
         file_extension = os.getenv('FILE_EXTENSION')
         t_delta = os.getenv('TIME_DELTA')
 
-        if not local_dir or not remote_dir:
+        if not local_dir_list or not remote_dir:
             raise ValueError('LOCAL_PATH and REMOTE_PATH must be set in env')
+        local_dir_list = local_dir_list.split(';')
         manager = SFTPManager(config)
 
         all_files: List[Path] = []
-        if file_extension:
-            all_files = FileManager.fetch_files_filtered_by_extension(
-                directory=local_dir,
-                extension=file_extension
-            )
-        else:
-            all_files = FileManager.fetch_files(directory=local_dir)
 
-        if t_delta:
-            target_day = datetime.now() - timedelta(days=int(t_delta))
-            all_files = FileManager.filter_files_by_date(all_files, target_day)
+        for local_dir in local_dir_list:
+            fetched_files: List[Path] = []
+            if file_extension:
+                fetched_files = FileManager.fetch_files_filtered_by_extension(
+                    directory=local_dir,
+                    extension=file_extension
+                )
+            else:
+                fetched_files = FileManager.fetch_files(directory=local_dir)
+
+            if t_delta:
+                target_day = datetime.now() - timedelta(days=int(t_delta))
+                fetched_files = FileManager.filter_files_by_date(
+                    fetched_files,
+                    target_day,
+                )
+            all_files.extend(fetched_files)
 
         with manager as sftp:
             for file in all_files:
@@ -65,6 +74,9 @@ def scheduled_task():
                     local_path=file,
                     remote_path=f'{remote_dir}/{file.name}',
                 )
+            sftp.list_files(remote_dir)
+
+        print(f'Scheduled SFTP file transfer completed at {datetime.now()}.')
 
     except Exception as e:
         print(e)
@@ -75,4 +87,7 @@ app.include_group(group)
 
 
 if __name__ == '__main__':
+    print('Starting scheduled SFTP file transfer...')
+    print('It will run every day at 00:01:01 (America/Recife timezone).')
+    print('Press Ctrl+C to exit.')
     asyncio.run(app.serve())
